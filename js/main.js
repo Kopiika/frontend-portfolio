84631a1f89665383a35818f00f540d2cd1ee27a1
Eleonora <eleonora.kopiika@gmail.com>
2026-01-19 09:44:13 +0200

    new links

--- js/main.js
+++ js/main.js
@@ -6,26 +6,6 @@ import "contacts-form.js";
 import "menu.js";
 import "scroll-toTopBtn.js";
 
-
-/* Smooth scrolling */
- document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-	anchor.addEventListener("click", function (e) {
-	  e.preventDefault();
- 
-	  const targetId = this.getAttribute("href").substring(1);
-	  const target = document.getElementById(targetId);
- 
-	  const header = document.getElementById("site-header");
-	  const headerHeight = header.offsetHeight;
- 
-	  const targetPosition = target.getBoundingClientRect().top + window.scrollY - headerHeight;
- 
-	  window.scrollTo({
-		 top: targetPosition,
-		 behavior: "smooth"
-	  });
-	});
- });
  
  
  
