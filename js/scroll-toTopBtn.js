ea8d0bb14ca44fff2401caef0ab5061a4f85eb67
Eleonora <eleonora.kopiika@gmail.com>
2026-01-19 09:45:51 +0200

    new links

--- js/scroll-toTopBtn.js
+++ js/scroll-toTopBtn.js
@@ -1,23 +1,4 @@
-/* Smooth scrolling */
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-	anchor.addEventListener("click", function (e) {
-	  e.preventDefault();
- 
-	  const targetId = this.getAttribute("href").substring(1);
-	  const target = document.getElementById(targetId);
- 
-	  const header = document.getElementById("site-header");
-	  const headerHeight = header.offsetHeight;
- 
-	  const targetPosition = target.getBoundingClientRect().top + window.scrollY - headerHeight;
- 
-	  window.scrollTo({
-		 top: targetPosition,
-		 behavior: "smooth"
-	  });
-	});
- });
- 
+
 /* Scroll to Top Button */
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
