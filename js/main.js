0e4850c425d42539128cd09f28d810d9367ea817
Eleonora <eleonora.kopiika@gmail.com>
2026-01-19 09:41:11 +0200

    new links

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-import "./theme.js";
-import "./typewriter.js";
-import "./skils-animate.js";
-import "./modal.js";
-import "./contacts-form.js";
-import "./menu.js";
-import "./scroll-toTopBtn.js";
+import "theme.js";
+import "typewriter.js";
+import "skils-animate.js";
+import "modal.js";
+import "contacts-form.js";
+import "menu.js";
+import "scroll-toTopBtn.js";
 
 
 /* Smooth scrolling */
